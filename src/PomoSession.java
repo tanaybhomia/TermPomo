71e2fe213f59f6e58f71c686c125054c400972c3
Tanay Bhomia <bhomia123456789@gmail.com>
2025-05-14 07:42:09 +0530

    Added and removed files

---
+++ src/PomoSession.java
@@ -0,0 +1,22 @@
+public class PomoSession {
+
+    private String mode;
+    private int duration;
+
+    public PomoSession(String mode, int duration) {
+        this.mode = mode.toLowerCase();
+        this.duration = duration;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
